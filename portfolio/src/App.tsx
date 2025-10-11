131233a0b1cbc291b0770c0c6180f7690f0f82bd
AnaPule <morwetsana.pule@gmail.com>
2025-10-11 11:24:21 +0200

    initial commit - basic website set up, with postcss and tailwind

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+import { useState } from 'react'
+
+
+//components
+
+//packages
+import { MotionConfig } from 'framer-motion'
+
+function App() {
+
+  return (
+    <>
+      <MotionConfig>
+
+      </MotionConfig>
+    </>
+  )
+}
+
+export default App
---
+++ portfolio/vite.config.ts
@@ -0,0 +1,23 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: process.env.BASE_PATH || '/',
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@models': resolve(__dirname, './src/models'),
+      '@components': resolve(__dirname, './src/components'),
+      '@utils': resolve(__dirname, './src/utils'),
+    }
+  },
+  build: {
+    outDir: 'dist'
+  }
+})
